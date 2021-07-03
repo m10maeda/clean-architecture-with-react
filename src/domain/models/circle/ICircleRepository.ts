b6a46449701bc454741e79553afee9c5e6290ccb
m10maeda <mio.maeda.kari@gmail.com>
2021-07-03 19:25:13 +0900

    Fix param name

--- src/domain/models/circle/ICircleRepository.ts
+++ src/domain/models/circle/ICircleRepository.ts
@@ -4,7 +4,7 @@ import CircleName from './CircleName';
 
 export interface ICircleRepository {
   find(id: CircleId): Promise<Circle | undefined>;
-  find(id: CircleName): Promise<Circle | undefined>;
+  find(name: CircleName): Promise<Circle | undefined>;
   findAll(): Promise<Iterable<Circle>>;
   save(circle: Circle): Promise<void>;
   delete(circle: Circle): Promise<void>;
