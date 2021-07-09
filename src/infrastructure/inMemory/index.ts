33171b7b57a74210cd63d7be6e4fa42c89981861
m10maeda <mio.maeda.kari@gmail.com>
2021-07-09 23:52:47 +0900

    Add in memory infrastructure index

---
+++ src/infrastructure/inMemory/index.ts
@@ -0,0 +1,5 @@
+export { default as InMemoryCircleDaoImpl } from './InMemoryCircleDaoImpl';
+export { default as InMemoryCircleFactory } from './InMemoryCircleFactory';
+export { default as InMemoryUserDaoImpl } from './InMemoryUserDaoImpl';
+export { default as InMemoryUserFactory } from './InMemoryUserFactory';
+export { default as SerialNumberAssigner } from './SerialNumberAssigner';
