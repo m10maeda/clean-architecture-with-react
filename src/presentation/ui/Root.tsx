1dab42a7e0feb4469bd2e51a0b4805276e85bc37
m10maeda <mio.maeda.kari@gmail.com>
2021-07-11 00:12:05 +0900

    Make redux store possible to provide for React

--- src/presentation/ui/Root.tsx
+++ src/presentation/ui/Root.tsx
@@ -1,6 +1,14 @@
 import React, { VFC } from 'react';
+import { Provider } from 'react-redux';
+import createStore from '../state/createStore';
 import Router from './Router';
 
-const Root: VFC = () => <Router />;
+const store = createStore();
+
+const Root: VFC = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
 
 export default Root;
