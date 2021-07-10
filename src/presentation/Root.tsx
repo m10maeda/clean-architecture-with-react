ba01e4c36cda8c94dca3c3ae84f34308709f3076
m10maeda <mio.maeda.kari@gmail.com>
2021-07-11 00:14:02 +0900

    Move presentaion root component

--- src/presentation/Root.tsx
+++ src/presentation/Root.tsx
@@ -1,7 +1,7 @@
 import React, { VFC } from 'react';
 import { Provider } from 'react-redux';
-import createStore from '../state/createStore';
-import Router from './Router';
+import createStore from './state/createStore';
+import Router from './ui/Router';
 
 const store = createStore();
 
