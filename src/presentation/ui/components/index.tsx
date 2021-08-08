d65fe0fa4ed7d2a0d18f23221b42bc13a77da400
m10maeda <mio.maeda.kari@gmail.com>
2021-08-08 23:36:11 +0900

    Add FormControl

--- src/presentation/ui/components/index.tsx
+++ src/presentation/ui/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Container } from './Container';
+export { default as FormControl } from './FormControl';
 export { default as ListGroup } from './ListGroup';
 export { default as PageHeader } from './PageHeader';
 export { default as Table } from './Table';
