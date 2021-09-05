927f544d17e42e847b9991839447482d2ab0480e
m10maeda <mio.maeda.kari@gmail.com>
2021-09-05 22:17:55 +0900

    Implement NotFound page

--- src/presentation/ui/pages/NotFoundPage/NotFoundPage.tsx
+++ src/presentation/ui/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,16 @@
-import React, { VFC } from 'react';
+import React, { useEffect, VFC } from 'react';
+import { PageHeader } from '../../components';
 
-const NotFoundPage: VFC = () => <div>NotFoundPage</div>;
+const NotFoundPage: VFC = () => {
+  useEffect(() => {
+    document.title = 'Not found';
+  }, []);
+
+  return (
+    <>
+      <PageHeader>Not Found</PageHeader>
+    </>
+  );
+};
 
 export default NotFoundPage;
