aceb16f01da18b8d4d784270884fec04757684cc
m10maeda <mio.maeda.kari@gmail.com>
2021-09-05 22:21:35 +0900

    Add Feedback with FormGroup

--- src/presentation/ui/components/FormGroup/index.tsx
+++ src/presentation/ui/components/FormGroup/index.tsx
@@ -1,8 +1,10 @@
 import Controls from './Controls';
+import Feedback from './Feedback';
 import FormGroup from './FormGroup';
 import Label from './Label';
 
 export default Object.assign(FormGroup, {
   Label,
   Controls,
+  Feedback,
 });
